2081c609bf96a02c59ae3ae5e7e2d388ac12bab6
Simon Templer <simon@wetransform.to>
2015-07-27 10:59:11 +0200

    project template as generated with yeoman
    see https://github.com/cgross/generator-cg-angular

---
+++ app.js
@@ -0,0 +1,23 @@
+angular.module('angularSample', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
+
+angular.module('angularSample').config(function($stateProvider, $urlRouterProvider) {
+
+    /* Add New States Above */
+    $urlRouterProvider.otherwise('/home');
+
+});
+
+angular.module('angularSample').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
